8a68d5993972823d84a79561f72dc270d070f710
soundariya-2212 <151667083+soundariya-2212@users.noreply.github.com>
2024-09-17 15:48:56 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -28,11 +28,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-toastify'],
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
